6fc31924bdc6d8ce45c497e7cb7231a93b51eb71
julianh65 <jhunt17159@gmail.com>
2022-05-10 21:29:47 -0400

    updates

--- bashba_parser/tests.py
+++ bashba_parser/tests.py
@@ -9,9 +9,8 @@ print("Building scanner...")
 os.system("ocamlbuild scantest.native")
 
 for i in range(1, 9):
-    print("Testing case " + str(i))
     if("error" in subprocess.getoutput(
             "./scantest.native < ./test_files/t{}".format(i))):
-        print("❌ Test failed")
+        print("Test case " + str(i) + " ❌ failed")
     else:
-        print("✅ Test passed!")
+        print("Test case " + str(i) + " ✅ passed!")
