42a6434768f2337591b5f63b5bcfbe45eb94df56
ben <yy3028@columbia.edu>
2022-05-15 23:13:56 -0400

    codegen test

--- bashba_parser/test_codegen.py
+++ bashba_parser/test_codegen.py
@@ -6,13 +6,16 @@ print("-- Testing codegen --")
 print("Rebuilding everything")
 os.system("rm a.out")
 os.system("rm bashba.native")
+os.system("ocamlbuild -clean")
 print("Building bashba...")
 os.system("ocamlbuild -pkgs llvm bashba.native")
 os.system("export PATH=/usr/local/opt/llvm/bin:$PATH")
 
 for filename in os.listdir("./codegen_test_files"):
-    if("error" in subprocess.getoutput(
-            "./bashba.native -l {} > a.out".format(filename))):
+    out = subprocess.getoutput(
+            "./bashba.native -l codegen_test_files/{} > a.out".format(filename))
+    if("error" in out):
+        print(out)
         print("Test case " + filename + " ❌ failed")
     else:
         print("Test case " + filename + " ✅ passed!")
